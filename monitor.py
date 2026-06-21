2a3838797587c4c585f7d7e49c5bea4cc9d9cea7
DIVYA-V0111 <divyavenkatesh91080@gmail.com>
2026-06-21 06:15:33 +0000

    add email alerting feature

--- monitor.py
+++ monitor.py
@@ -1,6 +1,8 @@
 import psutil
 import datetime
-import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 # Thresholds
 CPU_THRESHOLD = 80
@@ -9,6 +11,27 @@ DISK_THRESHOLD = 90
 
 # Log file
 LOG_FILE = "monitor.log"
+
+# Email settings
+SENDER_EMAIL = "divyavenkatesh91080@gmail.com"
+RECEIVER_EMAIL = "divyavenkatesh91080@gmail.com"
+APP_PASSWORD = "owztvrhnfqxhzrph"
+
+def send_email(subject, body):
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = SENDER_EMAIL
+        msg['To'] = RECEIVER_EMAIL
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(SENDER_EMAIL, APP_PASSWORD)
+        server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, msg.as_string())
+        server.quit()
+        log_message("Alert email sent successfully!")
+    except Exception as e:
+        log_message(f"Failed to send email: {e}")
 
 def get_cpu():
     return psutil.cpu_percent(interval=1)
@@ -31,10 +54,22 @@ def log_message(message):
 def check_alerts(cpu, memory, disk):
     if cpu > CPU_THRESHOLD:
         log_message(f"ALERT! CPU usage is high: {cpu}%")
+        send_email(
+            "ALERT CPU - Health Monitor",
+            f"CPU usage is high: {cpu}%\nTime: {datetime.datetime.now()}"
+        )
     if memory > MEMORY_THRESHOLD:
         log_message(f"ALERT! Memory usage is high: {memory}%")
+        send_email(
+            "ALERT Memory - Health Monitor",
+            f"Memory usage is high: {memory}%\nTime: {datetime.datetime.now()}"
+        )
     if disk > DISK_THRESHOLD:
         log_message(f"ALERT! Disk usage is high: {disk}%")
+        send_email(
+            "ALERT Disk - Health Monitor",
+            f"Disk usage is high: {disk}%\nTime: {datetime.datetime.now()}"
+        )
 
 def monitor():
     log_message("--- Health Check Started ---")
